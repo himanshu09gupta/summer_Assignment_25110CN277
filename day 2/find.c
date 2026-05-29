49f0ab694d378e73d880218c50352872d07c52f9
himanshu09gupta <guptahimaanshu935@gmail.com>
2026-05-29 22:03:34 +0530

    Add files via upload

---
+++ day 2/find.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    int number, digit, product = 1;
+
+   
+    printf("Enter a number: ");
+    scanf("%d", &number);
+
+    
+    while (number != 0) {
+        digit = number % 10;      
+        product = product * digit; 
+        number = number / 10;     
+    }
+
+    
+    printf("Product of digits = %d\n", product);
+
+    return 0;
+}
