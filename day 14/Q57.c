f324b42565df10f1b270dbe2bbfa423091d02978
himanshu09gupta <guptahimaanshu935@gmail.com>
2026-06-22 23:58:57 +0530

    Add files via upload

---
+++ day 14/Q57.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int arr[] = {10, 20, 30, 40, 50};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int temp;
+
+    for (int i = 0; i < n / 2; i++) {
+        temp = arr[i];
+        arr[i] = arr[n - 1 - i];
+        arr[n - 1 - i] = temp;
+    }
+
+    printf("Reversed Array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
