52c4be857d740d7d276cad3422386e334b939c7f
himanshu09gupta <guptahimaanshu935@gmail.com>
2026-06-24 23:59:57 +0530

    Add files via upload

---
+++ day 23/Q92.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    char str[100];
+    int freq[256] = {0};
+    int i, max = 0;
+    char maxChar;
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    for(i = 0; str[i] != '\0'; i++) {
+        freq[(unsigned char)str[i]]++;
+    }
+
+    for(i = 0; i < 256; i++) {
+        if(freq[i] > max) {
+            max = freq[i];
+            maxChar = i;
+        }
+    }
+
+    printf("Maximum occurring character: %c\n", maxChar);
+    printf("Frequency: %d\n", max);
+
+    return 0;
+}
