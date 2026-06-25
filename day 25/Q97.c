e33325c2c72ccb54c93c12f55238331bf94b25ea
himanshu09gupta <guptahimaanshu935@gmail.com>
2026-06-25 23:24:14 +0530

    Add files via upload

---
+++ day 25/Q97.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main() {
+    int a[] = {1, 3, 5, 7};
+    int b[] = {2, 4, 6, 8};
+
+    int n = 4, m = 4;
+    int c[n + m];
+
+    int i = 0, j = 0, k = 0;
+
+    while (i < n && j < m) {
+        if (a[i] < b[j])
+            c[k++] = a[i++];
+        else
+            c[k++] = b[j++];
+    }
+
+    while (i < n)
+        c[k++] = a[i++];
+
+    while (j < m)
+        c[k++] = b[j++];
+
+    printf("Merged Array: ");
+    for (i = 0; i < n + m; i++)
+        printf("%d ", c[i]);
+
+    return 0;
+}
---
+++ day 25/Q99.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char names[5][20], temp[20];
+
+    printf("Enter 5 names:\n");
+    for (int i = 0; i < 5; i++)
+        scanf("%s", names[i]);
+
+    for (int i = 0; i < 4; i++) {
+        for (int j = i + 1; j < 5; j++) {
+            if (strcmp(names[i], names[j]) > 0) {
+                strcpy(temp, names[i]);
+                strcpy(names[i], names[j]);
+                strcpy(names[j], temp);
+            }
+        }
+    }
+
+    printf("\nNames in Alphabetical Order:\n");
+    for (int i = 0; i < 5; i++)
+        printf("%s\n", names[i]);
+
+    return 0;
+}
